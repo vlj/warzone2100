129fec1cccdc5bc98d76b7da639ba54a18634929
Vincent Lejeune <vljn.ovi@gmail.com>
2017-03-19 19:23:00 +0100

    more

--- lib/ivis_opengl/bitimage.h
+++ lib/ivis_opengl/bitimage.h
@@ -20,8 +20,7 @@
 #ifndef __INCLUDED_BITIMAGE__
 #define __INCLUDED_BITIMAGE__
 
-#include "ivisdef.h"
-#include "piedef.h"
+#include "imageFile.h"
 
 static inline WZ_DECL_PURE unsigned short iV_GetImageWidth(const IMAGEFILE *ImageFile, const unsigned short ID)
 {

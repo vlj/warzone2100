96aff1bd88bb556dfc766a0dd4464ddf03b6818e
vlj <vljn.ovi@gmail.com>
2017-08-23 19:35:15 +0200

    more appveyor

--- get-dependencies.ps1
+++ get-dependencies.ps1
@@ -1,5 +1,5 @@
 git clone https://github.com/Microsoft/vcpkg.git;
 pushd vcpkg;
 .\bootstrap-vcpkg.bat;
-.\vcpkg install vorbis harfbuzz libiconv libogg libtheora libvorbis libpng openal-soft openssl physfs sdl2
+.\vcpkg install harfbuzz libiconv libogg libtheora libvorbis libpng openal-soft openssl physfs sdl2
 popd;

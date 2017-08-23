ab879e2d41f149f4cc5d2ca0924b4d48dfcf5147
vlj <vljn.ovi@gmail.com>
2017-08-23 19:29:29 +0200

    appveyor fixes

---
+++ get-dependencies.ps1
@@ -0,0 +1,5 @@
+git clone https://github.com/Microsoft/vcpkg.git;
+pushd vcpkg;
+.\bootstrap-vcpkg.bat;
+.\vcpkg install vorbis harfbuzz libiconv libogg libtheora libvorbis libpng openal-soft openssl physfs sdl2
+popd;

a3070aab7b67dbfb9c1d070db85b6ea0e9f8d776
Vincent Lejeune <vljn.ovi@gmail.com>
2018-05-01 00:14:51 +0200

    cleaning

--- tests/modeltest.cpp
+++ tests/modeltest.cpp
@@ -26,8 +26,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <gtest/gtest.h>
-#include <array>
-#include "../3rdparty/glm/glm.hpp"
 #include <filesystem>
 #include <range/v3/all.hpp>
 #include <fstream>
@@ -40,136 +38,134 @@ constexpr auto iV_IMD_TEXANIM = 0x00004000;
 constexpr auto iV_IMD_TCMASK = 0x00010000;
 constexpr auto MAX_POLYGON_SIZE = 3u; // the game can't handle more
 
-struct WZ_FACE {
-	std::array<int, MAX_POLYGON_SIZE> index;
-	std::array<glm::ivec2, MAX_POLYGON_SIZE> texCoord;
-	int vertices;
-	int frames, rate, width, height; // animation data
-	bool cull;
-};
-
-struct WZ_POSITION {
-	int x, y, z, reindex;
-	bool dupe;
-};
-
-static void check_pie(const fs::path& input)
+namespace
 {
-  int num, x, y, z, levels, level;
-  std::ifstream fp(input);
-  std::string s;
-  std::string keyword;
 
-  EXPECT_TRUE(fp >> keyword >> x);
-  EXPECT_EQ(keyword, "PIE") << "Bad PIE file";
-  EXPECT_FALSE(x != 2 && x != 3) << "Unknown PIE version";
-
-  EXPECT_TRUE(fp >> keyword >> std::hex >> x);
-  EXPECT_EQ(keyword, "TYPE") << "Bad TYPE directive";
-  EXPECT_FALSE(!(x & iV_IMD_TEX)) << "Missing texture flag";
-
-  EXPECT_TRUE(fp >> keyword >> std::hex >> z >> s >> x >> y);
-  EXPECT_EQ(keyword, "TEXTURE") << "Bad TEXTURE directive";
-
-  // Either event or level
-  EXPECT_TRUE(fp >> keyword);
-  while (keyword == "EVENT")
+  void check_pie(const fs::path& input)
   {
-    EXPECT_TRUE(fp >> x >> s);
-    EXPECT_FALSE(x < 1 || x > 3) << "Bad type for EVENT directive";
-    fp >> keyword;
+    int x, y, z, levels;
+    std::ifstream fp(input);
+    std::string s;
+    std::string keyword;
+
+    EXPECT_TRUE(fp >> keyword >> x);
+    EXPECT_EQ(keyword, "PIE") << "Bad PIE file";
+    EXPECT_FALSE(x != 2 && x != 3) << "Unknown PIE version";
+
+    EXPECT_TRUE(fp >> keyword >> std::hex >> x);
+    EXPECT_EQ(keyword, "TYPE") << "Bad TYPE directive";
+    EXPECT_FALSE(!(x & iV_IMD_TEX)) << "Missing texture flag";
+
+    EXPECT_TRUE(fp >> keyword >> std::hex >> z >> s >> x >> y);
+    EXPECT_EQ(keyword, "TEXTURE") << "Bad TEXTURE directive";
+
+    // Either event or level
+    EXPECT_TRUE(fp >> keyword);
+    while (keyword == "EVENT")
+    {
+      EXPECT_TRUE(fp >> x >> s);
+      EXPECT_FALSE(x < 1 || x > 3) << "Bad type for EVENT directive";
+      fp >> keyword;
+    }
+
+    EXPECT_EQ(keyword, "LEVELS") << "Bad LEVELS directive";
+    EXPECT_TRUE(fp >> std::dec >> levels);
+
+    EXPECT_TRUE(fp >> keyword);
+    for (int level = 0; level < levels; level++)
+    {
+      int points, faces;
+
+      EXPECT_EQ(keyword, "LEVEL") << "Bad LEVEL directive  in " << input.string() << " for level " << level + 1;
+      EXPECT_TRUE(fp >> std::dec >> x);
+      EXPECT_EQ(x, level + 1) << "LEVEL directive  was " << x << " should be " << level + 1;
+
+      EXPECT_TRUE(fp >> keyword >> std::dec >> points);
+      EXPECT_EQ(keyword, "POINTS") << "Bad POINTS directive";
+
+      for (int j = 0; j < points; j++)
+      {
+        double a, b, c;
+        EXPECT_TRUE(fp >> a >> b >> c);
+      }
+      EXPECT_TRUE(fp >> keyword >> faces);
+      EXPECT_EQ(keyword, "POLYGONS") << "Bad POLYGONS directive in " << input.string() << " in level " << x;
+
+      for (int j = 0; j < faces; ++j)
+      {
+        int k;
+        unsigned int flags;
+
+        EXPECT_TRUE(fp >> std::hex >> flags);
+
+        EXPECT_FALSE(!(flags & iV_IMD_TEX)) << "in " << input.string() << " Bad polygon flag entry level" << level << " number " << j << "- no texture flag!";
+        EXPECT_FALSE(flags & iV_IMD_XNOCUL) << "in" << input.string() << "Bad polygon flag entry level " << level << " number " << j << " - face culling not supported anymore!";
+        EXPECT_TRUE(fp >> std::dec >> k);
+        EXPECT_EQ(k, 3) << "Bad POLYGONS vertices entry level " << level << " number " << j << "-- non-triangle polygon found";
+
+        // Read in vertex indices and texture coordinates
+        for (int k = 0; k < 3; k++)
+        {
+          int l;
+          EXPECT_TRUE(fp >> l);
+        }
+        if (flags & iV_IMD_TEXANIM)
+        {
+          int frames, rate;
+          float width, height;
+          EXPECT_TRUE(fp >> std::dec >> frames >> rate >> width >> height);
+          EXPECT_GT(frames, 1) << "Level " << level << " Polygon " << j << "has a single animation frame";
+        }
+        for (int k = 0; k < 3; k++)
+        {
+          double t, u;
+          EXPECT_TRUE(fp >> t >> u);
+        }
+      }
+
+      fp >> keyword;
+      if (keyword == "CONNECTORS")
+      {
+        EXPECT_TRUE(fp >> std::dec >> x);
+        EXPECT_GE(x, 0) << "Bad CONNECTORS directive in level " << level;
+        for (int j = 0; j < x; ++j)
+        {
+          int a, b, c;
+
+          EXPECT_TRUE(fp >> a >> b >> c);
+        }
+        fp >> keyword;
+      }
+      if (keyword == "ANIMOBJECT")
+      {
+        int unused0, unused1;
+        EXPECT_TRUE(fp >> unused0 >> unused1 >> x);
+        EXPECT_GE(x, 0) << "Bad ANIMOBJECT directive in level " << level;
+        for (int j = 0; j < x; ++j)
+        {
+          int frame;
+          float xpos, ypos, zpos, xrot, yrot, zrot, xscale, yscale, zscale;
+
+          EXPECT_TRUE(fp >> std::dec >> frame >> xpos >> ypos >> zpos >> xrot >> yrot >> zrot >> xscale >> yscale >> zscale);
+        }
+        fp >> keyword;
+      }
+    }
   }
 
-  EXPECT_EQ(keyword, "LEVELS") << "Bad LEVELS directive";
-  EXPECT_TRUE(fp >> std::dec >> levels);
-
-  EXPECT_TRUE(fp >> keyword);
-	for (int level = 0; level < levels; level++)
-	{
-		int j, points, faces;
-
-    EXPECT_EQ(keyword, "LEVEL") << "Bad LEVEL directive  in " << input.string() << " for level " << level + 1;
-    EXPECT_TRUE(fp >> std::dec >> x);
-    EXPECT_EQ(x, level + 1) << "LEVEL directive  was " << x << " should be " << level + 1;
-
-    EXPECT_TRUE(fp >> keyword >> std::dec >> points);
-    EXPECT_EQ(keyword, "POINTS") << "Bad POINTS directive";
-
-		for (int j = 0; j < points; j++)
-		{
-			double a, b, c;
-      EXPECT_TRUE(fp >> a >> b >> c);
-    }
-    EXPECT_TRUE(fp >> keyword >> faces);
-    EXPECT_EQ(keyword, "POLYGONS") << "Bad POLYGONS directive in " << input.string() << " in level " << x;
-
-		for (int j = 0; j < faces; ++j)
-		{
-			int k;
-			unsigned int flags;
-
-      EXPECT_TRUE(fp >> std::hex >> flags);
-
-      EXPECT_FALSE(!(flags & iV_IMD_TEX)) << "in " << input.string() << " Bad polygon flag entry level" << level << " number " << j << "- no texture flag!";
-      EXPECT_FALSE(flags & iV_IMD_XNOCUL) << "in" << input.string() << "Bad polygon flag entry level " << level << " number " << j << " - face culling not supported anymore!";
-      EXPECT_TRUE(fp >> std::dec >> k);
-      EXPECT_EQ(k, 3) << "Bad POLYGONS vertices entry level " << level << " number " << j << "-- non-triangle polygon found";
-
-			// Read in vertex indices and texture coordinates
-			for (int k = 0; k < 3; k++)
-			{
-				int l;
-        EXPECT_TRUE(fp >> l);
-			}
-			if (flags & iV_IMD_TEXANIM)
-			{
-				int frames, rate;
-        float width, height;
-        EXPECT_TRUE(fp >> std::dec >> frames >> rate >> width >> height);
-        EXPECT_GT(frames, 1) << "Level " << level << " Polygon " << j << "has a single animation frame";
-			}
-			for (int k = 0; k < 3; k++)
-			{
-				double t, u;
-        EXPECT_TRUE(fp >> t >> u);
-			}
-		}
-
-    fp >> keyword;
-    if (keyword == "CONNECTORS")
-		{
-      EXPECT_TRUE(fp >> std::dec >> x);
-      EXPECT_GE(x, 0) << "Bad CONNECTORS directive in level " << level;
-			for (int j = 0; j < x; ++j)
-			{
-				int a, b, c;
-
-        EXPECT_TRUE(fp >> a >> b >> c);
-			}
-      fp >> keyword;
-		}
-		if (keyword == "ANIMOBJECT")
-		{
-      int unused0, unused1;
-      EXPECT_TRUE(fp >> unused0 >> unused1 >> x);
-      EXPECT_GE(x, 0) << "Bad ANIMOBJECT directive in level " << level;
-			for (int j = 0; j < x; ++j)
-			{
-				int frame;
-				float xpos, ypos, zpos, xrot, yrot, zrot, xscale, yscale, zscale;
-
-        EXPECT_TRUE(fp >> std::dec >> frame >> xpos >> ypos >> zpos >> xrot >> yrot >> zrot >> xscale >> yscale >> zscale);
-			}
-      fp >> keyword;
-		}
-	}
+  auto get_model_files = [](const auto& datapath) {
+    return fs::recursive_directory_iterator(datapath) |
+      ranges::view::transform([](auto&& e) {return e.path(); }) |
+      ranges::view::filter([](auto&& p) { return p.extension() == fs::path(".pie"); });
+  };
 }
 
-auto get_model_files = [](const auto& datapath) {
-  return fs::recursive_directory_iterator(datapath) |
-    ranges::view::transform([](auto&& e) {return e.path(); }) |
-    ranges::view::filter([](auto&& p) { return p.extension() == fs::path(".pie"); });
-};
+TEST(modelTest, ModelsFound)
+{
+  int n = 0;
+  for (const auto& model_file : get_model_files(fs::current_path())) n++;
+  EXPECT_NE(n, 0) << "No model found";
+}
 
 TEST(modelTest, LoadModels)
 {
